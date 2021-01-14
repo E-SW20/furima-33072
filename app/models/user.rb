bedb55dfa37c3adcdcd94c6ddcca93120a737526
E-SW20 <saba8492@icloud.com>
2021-01-14 19:09:37 +0900

    ログイン、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :nickname, presence: true
   validates :email, presence: true
-  validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
+  validates_format_of :password, with: PASSWORD_REGEX, message: "６文字以上の半角英数字で書いてください"
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で書いてください"}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で書いてください"}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで書いてください"}
