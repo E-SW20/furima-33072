1fa2984824e1b9f7c76c507997e8c28b28a82a3b
E-SW20 <saba8492@icloud.com>
2021-01-18 14:09:20 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    PASSWORD_REGEX_1 = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    PASSWORD_REGEX_2 = /\A[ぁ-んァ-ン一-龥々]+\z/.freeze
+    PASSWORD_REGEX_3 = /\A[ァ-ヶー－]+\z/.freeze
     validates :nickname
     validates :email
-    validates_format_of :password, with: PASSWORD_REGEX, message: "６文字以上の半角英数字で書いてください"
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "全角で書いてください"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "全角で書いてください"}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで書いてください"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで書いてください"}
+    validates_format_of :password, with: PASSWORD_REGEX_1, message: "６文字以上の半角英数字で書いてください"
+    validates :last_name, format: { with: PASSWORD_REGEX_2, message: "全角で書いてください"}
+    validates :first_name, format: { with: PASSWORD_REGEX_2, message: "全角で書いてください"}
+    validates :last_name_kana, format: { with: PASSWORD_REGEX_3, message: "全角カタカナで書いてください"}
+    validates :first_name_kana, format: { with: PASSWORD_REGEX_3, message: "全角カタカナで書いてください"}
     validates :birthday
   end
 end
