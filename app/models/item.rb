f9b57e2760d0ded71ae3311b576468c588055f5c
E-SW20 <saba8492@icloud.com>
2021-01-19 12:57:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_price
+  belongs_to :prefectures
+  belongs_to :delivery_time
+
+  validates :image, :name, :explanation, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :state_id, numericality: { other_than: 1 } 
+  validates :delivery_price_id, numericality: { other_than: 1 } 
+  validates :prefectures_id, numericality: { other_than: 1 } 
+  validates :delivery_time_id, numericality: { other_than: 1 } 
 end
