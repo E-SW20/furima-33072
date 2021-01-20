3c1b7c497660acfac4011eb4df284b32a82f9034
E-SW20 <saba8492@icloud.com>
2021-01-20 16:40:33 +0900

    販売手数料・販売利益の計算結果表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1) 
+
+    const Profit = document.getElementById("profit");
+    Profit.innerHTML = Math.floor(inputValue - (inputValue * 0.1)) 
+  })
+  
+});
