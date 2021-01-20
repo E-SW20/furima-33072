3ae1d4745870a750b40cebdc9348ab8065e83a8e
E-SW20 <saba8492@icloud.com>
2021-01-20 18:27:23 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -57,12 +57,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "価格が299円以下だと登録できない" do
-        @item.price = "200"
+        @item.price = 200
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "価格が9999999円以上だと登録できない" do
-        @item.price = "20000000"
+        @item.price = 20000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
