4cfd6f3a3bf59d4bbd18541e196baa78584ea758
E-SW20 <saba8492@icloud.com>
2021-01-21 17:33:01 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    #@items = Item.all.order("created_at DESC")
     @items = Item.includes(:user).order("created_at DESC")
   end
   
