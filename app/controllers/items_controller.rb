d169cb406d36b14754e3522ed4469d2e011aecb7
E-SW20 <saba8492@icloud.com>
2021-01-25 17:05:26 +0900

    指摘の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    if @item.destroy && current_user.id == @item.user_id
+    if  current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path      
     end
   end
